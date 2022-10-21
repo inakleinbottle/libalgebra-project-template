7a925d845874b1e3b6357d9294369fb537c16d1e
inakleinbottle <admin@inakleinbottle.com>
2022-10-21 08:51:06 +0100

    Started adding command line options

--- la_helpers/la_helpers/la_helper_main.cpp
+++ la_helpers/la_helpers/la_helper_main.cpp
@@ -4,13 +4,42 @@
 
 #include "la_helpers.h"
 
-int main() {
+
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, const char** argv) {
+    unsigned width;
+    unsigned depth;
+
+    po::options_description descr("Options");
+    descr.add_options()
+            ("help", "Display the help message and quit")
+            ("width", po::value<unsigned>(&width)->default_value(0), "Set a width filter for examples")
+            ("depth", po::value<unsigned>(&depth)->default_value(0), "Set a depth filter for examples")
+            ("name", po::value<std::string>()->default_value(""), "Set a name filter for examples")
+            ("ctype", po::value<std::string>()->default_value(""), "Set a ctype filter for examples")
+            ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, descr), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << descr << '\n';
+        return EXIT_SUCCESS;
+    }
 
     const auto& list = alghelp::get_example_list();
 
     for (const auto& example : list) {
-        example->run();
+        try {
+            example->run();
+        } catch (std::exception& exc) {
+            std::cerr << exc.what() << '\n';
+        }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
